3b417d8815d162d7036ef6ba9319ede9ce00d469
faizazahmi <faizalemon021@gmail.com>
2024-04-16 01:43:26 +0700

    Initial commit

---
+++ app/src/main/java/com/example/aplikasitugasku/MainActivity4.java
@@ -0,0 +1,26 @@
+package com.example.aplikasitugasku;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+    }
+
+    public void imageButton4(View view) {
+        Intent intent = new Intent(MainActivity4.this, MainActivity2.class);
+        startActivity(intent);
+        ;
+    }
+}
